cbdfbaaa6d8088e4e8571ac9cee7b167842c3409
andrian-kars <andrian.karsanashvili@gmail.com>
2022-12-16 15:57:55 +0200

    :wrench: refresh add await

--- services/user-service.js
+++ services/user-service.js
@@ -74,7 +74,7 @@ class UserService {
     }
     const user = await UserModel.findById(userData.id);
     const userDto = new UserDto(user);
-    const tokens = tokenService.generateTokens({ ...userDto });
+    const tokens = await tokenService.generateTokens({ ...userDto });
 
     await tokenService.saveToken(userDto.id, tokens.refreshToken);
     return { ...tokens, user: userDto };
